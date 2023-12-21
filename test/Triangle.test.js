c79ff6e2c32b33ba749c61d48f146e8b0d87de64
Jescel James <jesceljames@gmail.com>
2023-12-21 00:18:32 -0500

    update triangle

--- test/Triangle.test.js
+++ test/Triangle.test.js
@@ -1,13 +1,15 @@
 const Triangle = require ('../lib/Triangle');
 
+describe ('Triangle class', () => { 
+    describe ('create a Triangle class', () => {
 
-describe ('create a Triangle class', () => {
+        it('should create a Triangle class', () => {
 
-    it('should create a Triangle class', () => {
+            const triangle = new Triangle();
+            
+            expect(triangle).toBeInstanceOf(Triangle)
+        })
 
-        const triangle = new Triangle();
-        
-        expect(triangle).toBeInstanceOf(Triangle)
     })
 
 })
