bab34d9407894db10fecadec823f5cd066814193
Jescel James <jesceljames@gmail.com>
2023-12-26 13:41:37 -0500

    .then added and svg function

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
+//DEPENDENCIES/REQUIRES
 const inquirer = require('inquirer');
 require('fs');
 
+//FUNCTIONS
 
-function askUser () {
-    return inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'radius',
-                        message: 'Enter the radius of the circle: ',
-                        default: 50
-                    }
+function generateSvgContent(answers) {
+    return `<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="100" cy="100" r="${answers.radius}" fill="${answers.color}" />
+    </svg>`;
+  }
+  
+function init () {
+    inquirer.prompt([
+            {
+                type: 'input',
+                name: 'radius',
+                message: 'Enter the radius of the circle: ',
+                default: 50
+            }
     ])
+
+            .then( function(answers) {
+                const svgContent = generateSvgContent(answers);
+
+            })
 }
 
-askUser();
+
+
+
+
+
+
+
+//INITIALIZE
+
+init();
