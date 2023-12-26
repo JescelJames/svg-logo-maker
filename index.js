3dc01c15ba4eb6acf72582d213b41c57c27c9df8
Jescel James <jesceljames@gmail.com>
2023-12-26 13:15:19 -0500

    askUser function works

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-require('inquirer');
+const inquirer = require('inquirer');
 require('fs');
+
+
+function askUser () {
+    return inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'radius',
+                        message: 'Enter the radius of the circle: ',
+                        default: 50
+                    }
+    ])
+}
+
+askUser();
