b0727e5f7b6b889876a0b077d719222ee3034628
Jescel James <jesceljames@gmail.com>
2023-12-26 15:09:07 -0500

    text inside image

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 function generateSvgContent(answers) {
     return `<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
         <circle cx="100" cy="100" r="${answers.radius}" fill="${answers.color}" />
+        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="20" fill="black">${answers.text}</text>
     </svg>`;
 }
 
