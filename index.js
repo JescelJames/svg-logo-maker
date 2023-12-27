8f73feb32b9aa90792a55c3d2f748543f83ff4e9
Jescel James <jesceljames@gmail.com>
2023-12-27 14:46:00 -0500

    first commit in branch

--- index.js
+++ index.js
@@ -39,12 +39,7 @@ function init() {
                 message: 'Enter the color of the text: ',
                
             },
-            {
-                type: 'list',
-                message: 'Select the shape of your logo: ',
-                name: 'shape',
-                choices: ['circle', 'triangle', 'square'],
-            },
+
             {
                 type: 'input',
                 name: 'radius',
