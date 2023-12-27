223a17023663e3d69998cfc3db91051865c42aac
Jescel James <jesceljames@gmail.com>
2023-12-27 14:41:19 -0500

    able to change text color with prompts

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const fs = require('fs');
 
 function generateSvgContent(answers) {
     return `<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="100" cy="100" r="${answers.radius}" fill="${answers.color}" />
-        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="20" fill="white">${answers.text}</text>
+        
+         <circle cx="100" cy="100" r="${answers.radius}" fill="${answers.color}" />
+        
+        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="20" fill="${answers.textcolor}">${answers.text}</text>
+        
+
+        
+       
     </svg>`;
 }
 
@@ -23,9 +29,21 @@ function init() {
             {
                 type: 'input',
                 name: 'text',
-                message: 'Enter up to 3 characters to display inside the circle: ',
+                message: 'Enter up the text to display up to 3 characters: ',
                 validate: input => input.length <= 3 || 'Text must be up to 3 characters only',
                 default: ''
+            },
+            {
+                type: 'input',
+                name: 'textcolor',
+                message: 'Enter the color of the text: ',
+               
+            },
+            {
+                type: 'list',
+                message: 'Select the shape of your logo: ',
+                name: 'shape',
+                choices: ['circle', 'triangle', 'square'],
             },
             {
                 type: 'input',
