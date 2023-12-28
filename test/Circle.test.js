24dcd596a62baa9ffec53916fd8095195ba6cc4e
Jescel James <jesceljames@gmail.com>
2023-12-28 05:17:29 -0500

    circle class test pass

--- test/Circle.test.js
+++ test/Circle.test.js
@@ -1,1 +1,11 @@
-const circle = require('../lib/Circle');
+const { default: test } = require('node:test');
+const Circle = require('../lib/Circle');
+
+describe('Create a Circle class', () => {
+    it('should create a Circle class', () => {
+        
+        const circle = new Circle();
+
+        expect (circle).toBeInstanceOf(Circle)
+    })
+})
