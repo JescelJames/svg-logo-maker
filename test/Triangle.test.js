cb53011f5527e507e6375db4be726794fda9dbe5
Jescel James <jesceljames@gmail.com>
2023-12-28 06:47:30 -0500

    test failing.. booo

---
+++ test/Triangle.test.js
@@ -0,0 +1,14 @@
+// shapeSvg = `<polygon points="100,10 40,290 160,290" fill="${answers.color}" />`;
+
+const { triggerAsyncId } = require('async_hooks');
+
+const Triangle = require('../lib/Triangle');
+
+
+describe('Triangle class color test', () => {
+    it('should render the correct color', () => {
+        const shape = new Triangle();
+        shape.setColor("blue");
+        expect(shape.render()).toEqual('<polygon points="100,10 40,290 160,290" fill="blue" />');
+    })
+})
